7718a6563bc201cb8898049fc47618e901f529d8
Valeriya Ripyanchina <v.ripyanchina@gmail.com>
2021-06-07 15:42:17 +0300

    \\ задача,коррект

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,6 +1,6 @@
 function truncate(str, maxlength) {
   // ваш код...
   return (str.length > maxlength) ? 
-  str.slice(0, maxlength - 1) + '...' : str;
+  str.slice(0, maxlength - 1) + '…' : str;
 }
 
