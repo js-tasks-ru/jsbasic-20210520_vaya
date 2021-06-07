7278f830dc963a53e67912526b3b5ce7b6fe7d5c
Valeriya Ripyanchina <v.ripyanchina@gmail.com>
2021-06-07 20:52:36 +0300

    \\ Решение 1 задачи 2 модуля

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,7 @@
 function sumSalary(salaries) {
   // ваш код...
+  let sum = 0;
+  Object.values(salaries).map((item) => isFinite(item) && typeof item !== "boolean" ? (sum += item) : false);
+  return sum;
 }
+
