37468e1751eeda3581ee444d449afa186e579a25
Valeriya Ripyanchina <v.ripyanchina@gmail.com>
2021-06-10 23:31:02 +0300

    \\ Решение 3 задачи 2 модуля

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,22 @@
 let calculator = {
   // ваш код
+  read() {
+    this.a = +prompt('a?', 3);
+    this.b = +prompt('b?', 5);
+  },
+
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  }
 };
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
