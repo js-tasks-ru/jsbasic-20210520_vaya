da27ab8d36bb458fd35fa4843d009375689491e0
Valeriya Ripyanchina <v.ripyanchina@gmail.com>
2021-06-11 22:26:08 +0300

    \\ Решение 5 задачи 3 модуля

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,20 @@
 function getMinMax(str) {
   // ваш код...
+  args = [...str]
+  .map(i => {if(isFinite(i) == true || i == "." || i == "-"){
+    return i
+  } else {
+    return " "}
+  })
+  .join("")
+  .split(" ")
+  .filter(i => i != "")
+  .map(i => Number(i));
+  let min = Math.min(...args);
+  let max = Math.max(...args);
+  let finalObj = {
+    min: min ,
+    max: max
+  };
+  return finalObj;
 }
