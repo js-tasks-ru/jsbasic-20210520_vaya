012256b22cfe63926d16ef818c8e3d2fa10b5786
Valeriya Ripyanchina <v.ripyanchina@gmail.com>
2021-06-11 19:46:50 +0300

    \\ Решение 4 задачи 3 модуля

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
 function showSalary(users, age) {
   // ваш код...
+  let userAge = users.filter((item) => item.age <= age);
+  let string = userAge.map((item) => item.name + ', ' + item.balance).join('\n');
+  return string;
 }
