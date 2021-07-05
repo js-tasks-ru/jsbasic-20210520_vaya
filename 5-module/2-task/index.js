5c8938c16a395c6db020baca8e73095cb1df8152
Valeriya Ripyanchina <v.ripyanchina@gmail.com>
2021-07-05 17:35:23 +0300

    \\ Решение 2 задачи 5 модуля

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
   // ваш код...
+  let button1 = document.querySelector(".toggle-text-button");
+  let div1 = document.querySelector("#text");
+  button1.addEventListener("click", ()=> {
+    div1.toggleAttribute("hidden");
+  });
 }
